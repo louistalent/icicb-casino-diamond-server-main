e446c13e43695f7d14c79c29212241761c252e4c
goodman <topdeveloper01234@gmail.com>
2024-05-10 16:35:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Diamond Server
+This is Diamond Game Server for Atari Casino Platform.
+Server's main communication method is socket communication.
+you can learn socket communication about this repositry.
+
+
+#Run
+1.Please install npm
+npm install
+2.Please start
+npm start
---
+++ server.js
@@ -0,0 +1,162 @@
+var express = require('express');
+var app = express();
+const axios = require("axios");
+require('dotenv').config();
+var bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+var cors = require('cors');
+const util = require('util');
+var server = require('http').createServer(app);
+var port = 80;
+var io = require('socket.io')(server);
+axios.defaults.headers.common["Authorization"] = process.env.SECRETCODE;
+
+gameSocket = null;
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(express.static(__dirname + "/build"));
+app.get("/*", function (req, res) {
+    res.sendFile(__dirname + "/build/index.html", function (err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
+});
+
+server.listen(port, function () {
+	console.log("server is running on " + port);
+});
+
+
+
+// Implement socket functionality
+gameSocket = io.on('connection', function (socket) {
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
+
+	socket.on('bet info', async(req) => {
+		var randomArray = [];
+		var betResult = [];
+		var counts = [];
+		var isBetting;
+		var amount;
+		var errorMessage = [];
+
+		console.log(req);	
+
+		try {
+			isBetting = req.isBetting;
+			betAmount = req.betAmount;
+			amount = req.amount - betAmount;
+
+			try{
+				await axios.post(process.env.PLATFORM_SERVER + "api/games/bet", {
+					token: req.token,
+					amount: req.betAmount
+				}); 
+			  }catch(err){
+				throw new Error("Bet Error")
+			  }
+
+			randomArray = CreateRandomArray();
+			console.log(randomArray)
+			counts = count_duplicate(randomArray);
+			var amountCross = getScore(counts);
+			var earnAmount = betAmount * amountCross;
+			amount += earnAmount;
+			console.log(amountCross, earnAmount, amount);
+
+			try{
+				await axios.post(process.env.PLATFORM_SERVER + "api/games/winlose", {
+				  token: req.token,
+				  amount: earnAmount,
+				  winState: true
+				});
+			  }catch(err){
+				throw new Error("Can't find server")
+			  }
+
+			betResult = {"amountCross":amountCross, "earnAmount":earnAmount, "amount":amount, "randomArray":randomArray};
+			socket.emit("game result", betResult)
+		} catch (err) {
+			errorMessage = {"errorMessage": err.message}
+			socket.emit("error message", errorMessage)
+		}
+	}); 	
+
+	console.log('socket connected: ' + socket.id);
+	socket.emit('connected', {});
+});
+
+function CreateRandomArray() {
+	var randomArray = [];
+	for (var i = 0; i < 5; i++) {
+		var randomNum = getRandomInt(7);
+		randomArray.push(randomNum);
+	}
+	return randomArray;
+}
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
+}
+
+function count_duplicate(randomArray) {
+	var counts = [];
+	randomArray.forEach((i) => { counts[i] = (counts[i] || 0) + 1 });
+	counts = counts.filter((c) => { if (c) return c });
+	counts = counts.sort();
+	return counts;
+}
+
+const getScore = (counts) => {
+	for (var score of scores) {
+		if (arrayEquals(counts, score.counts)) {
+			return score.rate;
+		}
+	}
+	return 0;
+}
+
+function arrayEquals(a, b) {
+	return Array.isArray(a) &&
+		Array.isArray(b) &&
+		a.length === b.length &&
+		a.every((val, index) => val === b[index]);
+}
+
+// score
+var scores = [
+	{
+		counts: [1, 1, 1, 1, 1],
+		rate: 0
+	},
+	{
+		counts: [1, 1, 1, 2],
+		rate: 0.1
+	},
+	{
+		counts: [1, 2, 2],
+		rate: 2
+	},
+	{
+		counts: [1, 1, 3],
+		rate: 3
+	},
+	{
+		counts: [2, 3],
+		rate: 4
+	},
+	{
+		counts: [1, 4],
+		rate: 5
+	},
+	{
+		counts: [5],
+		rate: 50
+	},
+]
